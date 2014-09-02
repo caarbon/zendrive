2c11b169ee61a56b25190bd37955c7a62c90bace
tmarshall <timothyjmarshall@gmail.com>
2014-09-02 16:48:26 -0700

    what tabs no, stop that

--- zendrive.js
+++ zendrive.js
@@ -2,72 +2,72 @@ var request = require('request');
 var url = 'http://api.zendrive.com';
 
 function v1(opts) {
-	this.key = opts.key;
+  this.key = opts.key;
 }
 
 v1.prototype.users = function(params, callback) {
-	if (arguments.length < 2) {
-		callback = params;
-		params = {};
-	}
+  if (arguments.length < 2) {
+    callback = params;
+    params = {};
+  }
 
-	makeRequest(params, 'users', callback);
+  makeRequest(params, 'users', callback);
 };
 
 v1.prototype.score = function(params, callback) {
-	if (arguments.length < 2) {
-		callback = params;
-		params = {};
-	}
-	
-	makeRequest(params, 'score', callback);
+  if (arguments.length < 2) {
+    callback = params;
+    params = {};
+  }
+  
+  makeRequest(params, 'score', callback);
 };
 
 v1.prototype.globalScore = function(params, callback) {
-	if (arguments.length < 2) {
-		callback = params;
-		params = {};
-	}
-	
-	makeRequest(params, 'global_score', callback);
+  if (arguments.length < 2) {
+    callback = params;
+    params = {};
+  }
+  
+  makeRequest(params, 'global_score', callback);
 };
 
 v1.prototype.userScore = function(id, params, callback) {
-	if (arguments.length < 3) {
-		callback = params;
-		params = {};
-	}
-	
-	makeRequest(params, ['user', id, 'score'].join('/'), callback);
+  if (arguments.length < 3) {
+    callback = params;
+    params = {};
+  }
+  
+  makeRequest(params, ['user', id, 'score'].join('/'), callback);
 };
 
 function makeRequest(params, path, callback) {
-	var preparedParams = {};
-	var key, actualKey, val;
+  var preparedParams = {};
+  var key, actualKey, val;
 
-	for (key in params) {
-		val = params[key];
+  for (key in params) {
+    val = params[key];
 
-		actualKey = key === 'start' || key === 'end' ? key + '_date' : key;
+    actualKey = key === 'start' || key === 'end' ? key + '_date' : key;
 
-		preparedParams[actualKey] = Array.isArray(val) ? val.join(',') :
-			key === 'start' || key === 'end' ? [ val.getFullYear(), val.getMonth() + 1, val.getDate() ].join('-') :
-			val;
-	}
+    preparedParams[actualKey] = Array.isArray(val) ? val.join(',') :
+      key === 'start' || key === 'end' ? [ val.getFullYear(), val.getMonth() + 1, val.getDate() ].join('-') :
+      val;
+  }
 
-	request({
-		url: [url, 'v1', path].join('/'),
-		method: 'get',
-		qs: preparedParams
-	});
+  request({
+    url: [url, 'v1', path].join('/'),
+    method: 'get',
+    qs: preparedParams
+  });
 }
 
 function zendrive(opts) {
-	this.v1 = new v1(opts);
+  this.v1 = new v1(opts);
 
-	for (var key in this.v1) {
-		this[key] = this[key] || this.v1[key];
-	}
+  for (var key in this.v1) {
+    this[key] = this[key] || this.v1[key];
+  }
 }
 
 module.exports = zendrive;
